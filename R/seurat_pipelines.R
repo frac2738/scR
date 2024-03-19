323d71ab084936055fcb894da0d6d0f0b8f13622
frac.2738 <frac.2738@gmail.com>
2024-03-19 09:56:53 +0100

    fix doublet detection

--- R/seurat_pipelines.R
+++ R/seurat_pipelines.R
@@ -70,9 +70,9 @@ run_qc_and_filtering <- function(exp_meta,sample_num,pipeline, minFeatures, pcDi
       cli_inform(message = " Setting seurat v5 to FALSE")
       options(Seurat.object.assay.version = "v5")
       
-      isSCT <- ifelse(pipeline == "SCT",TRUE,FALSE)
+      #isSCT <- ifelse(pipeline == "SCT",TRUE,FALSE)
       
-      doublets <- run_doubletFinder(raw_st, meta_column = "seurat_clusters", isSCT = isSCT, countMatrix = TRUE, seurat5 = seurat5)
+      doublets <- run_doubletFinder(raw_st, meta_column = "seurat_clusters", isSCT = FALSE, countMatrix = TRUE, seurat5 = seurat5)
       colnames(doublets@meta.data)[ncol(doublets@meta.data)] <- "isDoublet"
          
       #Idents(doublets) <- "isDoublet"
